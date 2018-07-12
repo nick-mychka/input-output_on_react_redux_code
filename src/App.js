00c2f8daf2798ab0f75dc233e7882e172f23d929
Nick Mychka <nickmychka@gmail.com>
2018-07-12 14:43:04 +0300

    added code of app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Persons from './containers/Persons';
+
+class App extends Component {
+  render() {
+    return (
+			<Persons />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/AddPerson/AddPerson.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+import './AddPerson.css';
+
+class AddPerson extends Component {
+	state = {
+		name: '',
+		age: ''
+	}
+
+	nameChangeHandler = (event) => {
+		this.setState({name: event.target.value});
+	}
+
+	ageChangeHandler = (event) => {
+		this.setState({age: event.target.value});
+	}
+
+	render() {
+		return(
+			<div className = "AddPerson">
+				<input 
+					className = "Input" 
+					type = "text" 
+					placeholder = "Name" onChange = {this.nameChangeHandler} 
+					value = {this.state.name} />
+				<input 
+					className = "Input" 
+					type = "number" 
+					placeholder = "Age" 
+					onChange = {this.ageChangeHandler} 
+					value = {this.state.age} />
+				<button className = "Button" onClick = {() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+			</div>
+		);
+	}
+}
+
+export default AddPerson;
